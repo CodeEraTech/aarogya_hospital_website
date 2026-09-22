e64247e1f12cd4003d6b5bba99eadaf175fbcdac
Vivek <codeeradevelopers@gmail.com>
2026-09-22 11:01:02 +0530

    chnages

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -11,6 +11,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets/hospital/css/site.css') }}?v={{ filemtime(public_path('assets/hospital/css/site.css')) }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 <a class="skip-link" href="#main">Skip to main content</a>
